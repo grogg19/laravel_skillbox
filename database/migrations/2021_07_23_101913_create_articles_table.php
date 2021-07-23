5075b31089ca8958be69aac85c1bb1c9407f4199
grogg19 <differ@list.ru>
2021-07-23 16:59:05 +0300

    добавлено поле slug

--- database/migrations/2021_07_23_101913_create_articles_table.php
+++ database/migrations/2021_07_23_101913_create_articles_table.php
@@ -16,6 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title', 191);
+            $table->string('slug')->unique();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
