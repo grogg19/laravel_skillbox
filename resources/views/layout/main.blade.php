01b6c23ae155340ad41d9255d42e22218118e2c8
grogg19 <differ@list.ru>
2021-07-23 14:22:33 +0300

    создан главный шаблон

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/blog/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <link href="{{ asset('css/blog.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+
+    @include('partials/header')
+
+    @include('partials/nav')
+
+</div>
+
+<main role="main" class="container">
+    <div class="row">
+        <div class="col-md-8 blog-main">
+            <h3 class="pb-3 mb-4 font-italic border-bottom">
+                From the Firehose
+            </h3>
+
+            @yield('content')
+
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+            </nav>
+
+        </div><!-- /.blog-main -->
+
+        @include('partials/sidebar')
+
+    </div><!-- /.row -->
+
+</main><!-- /.container -->
+
+@include('partials/footer')
+
+</body>
+</html>
