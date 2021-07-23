088e169f77390d1838277e5ce1242d9e3c4131ed
grogg19 <differ@list.ru>
2021-07-23 16:39:35 +0300

    добавлен метод getArticleBySlug()

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -4,15 +4,35 @@ namespace App\Repositories;
 
 use App\Models\Article;
 
+/**
+ * Class ArticleRepository
+ * @package App\Repositories
+ */
 class ArticleRepository
 {
+    /**
+     * @return mixed
+     */
     public static function listArticles()
     {
         return Article::latest()->get();
     }
 
-    public function getArticle($id)
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function getArticleById(int $id)
     {
         return Article::where('id', $id)->get();
+    }
+
+    /**
+     * @param string $slug
+     * @return mixed
+     */
+    public function getArticleBySlug(string $slug)
+    {
+        return Article::where('slug', $slug);
     }
 }
