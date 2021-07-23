869b855c32b10df3f560d857b0dd3e8c500b3ca4
grogg19 <differ@list.ru>
2021-07-23 16:59:49 +0300

    дбавлены новые маршруты

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'ArticlesController@index');
 
 Route::get('/contacts/', function () {
-    return view('contacts');
+    return view('contacts', ['title' => 'Контакты']);
 });
 
 Route::get('/about/', function () {
-    return view('about', ['title' => 'About']);
-});
-
-Route::get('/about/us/', function () {
-    return view('about', ['title' => 'About us']);
+    return view('about', ['title' => 'О нас']);
 });
