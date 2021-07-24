3a0bebc2abfe3945957cf760d57252df1b716aff
grogg19 <differ@list.ru>
2021-07-24 21:41:04 +0300

    добавлен вывод даты в шаблон

--- resources/views/articles/list.blade.php
+++ resources/views/articles/list.blade.php
@@ -2,8 +2,9 @@
     @foreach($articles as $article)
         <div class="blog-post">
             <h2 class="blog-post-title">{{ $article->title }}</h2>
+            <p class="blog-post-meta">{{ $article->created_at->toFormattedDateString() }}</p>
             <p>{{ $article->excerpt }}</p>
-            <a href="/articles{{ $article->slug }}">Читать статью</a>
+            <a href="/articles/{{ $article->slug }}">Читать статью</a>
         </div>
     @endforeach
 @endsection
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,5 +1,10 @@
-@extends('index')
+@extends('layout.main')
 
 @section('content')
-    <h1>Детальная страница статьи</h1>
+    <h1>{{ $article->title }}</h1>
+    <p class="blog-post-meta">{{ $article->created_at->toFormattedDateString() }}</p>
+    {{ $article->body }}
+    <p>
+        <a href="/">К списку статей</a>
+    </p>
 @endsection
