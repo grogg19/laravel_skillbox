efda650210ef2afc5a703dfd545f3bcd63f25730
grogg19 <differ@list.ru>
2021-07-25 21:12:29 +0300

    маршруты для Message

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/articles/create', 'ArticlesController@create');
 Route::post('/articles/store', 'ArticlesController@store');
 Route::get('/articles/{slug}', 'ArticlesController@show');
 
-Route::get('/contacts/', function () {
-    return view('contacts', ['title' => 'Контакты']);
-});
+Route::get('/feedback', 'MessagesController@index');
+Route::get('/contacts', 'MessagesController@create');
+Route::post('/contacts/store', 'MessagesController@store');
 
 Route::get('/about/', function () {
     return view('about', ['title' => 'О нас']);
