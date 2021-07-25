bcb140410a4879f89120cc51fca0926e92704b54
grogg19 <differ@list.ru>
2021-07-25 21:33:26 +0300

    Добавлен заголовок

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.main')
 
 @section('content')
+    <h3 class="pb-3 mb-4 font-italic border-bottom">
+        Детальная страница статьи
+    </h3>
     <h1>{{ $article->title }}</h1>
     <p class="blog-post-meta">{{ $article->created_at->toFormattedDateString() }}</p>
     {{ $article->body }}
