c08e028bcbc63f43e76384d143161a8f88b40ccc
grogg19 <differ@list.ru>
2021-07-26 14:30:24 +0300

    маршруты сделаны именованными

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'ArticlesController@index');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::post('/articles/store', 'ArticlesController@store');
-Route::get('/articles/{slug}', 'ArticlesController@show');
+Route::get('/', 'ArticlesController@index')->name('mainPage');
+Route::get('/articles/create', 'ArticlesController@create')->name('createArticle');
+Route::post('/articles/store', 'ArticlesController@store')->name('saveArticle');
+Route::get('/articles/{slug}', 'ArticlesController@show')->name('showArticle');
 
-Route::get('/feedback', 'MessagesController@index');
-Route::get('/contacts', 'MessagesController@create');
-Route::post('/contacts/store', 'MessagesController@store');
+Route::get('/feedback', 'MessagesController@index')->name('feedbackPage');
+Route::get('/contacts', 'MessagesController@create')->name('contactsPage');
+Route::post('/contacts/store', 'MessagesController@store')->name('saveMessage');
 
 Route::get('/about/', function () {
-    return view('about', ['title' => 'О нас']);
-});
+    return view('about');
+})->name('aboutPage');
