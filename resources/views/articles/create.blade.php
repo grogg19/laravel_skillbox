effce0d7111e09bea99b6726fe47d62af2efb5a3
grogg19 <differ@list.ru>
2021-07-26 14:32:17 +0300

    добавлен метод old() элементам в поля value

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -19,22 +19,22 @@
         @csrf
         <div class="form-group">
             <label for="title">Заголовок:</label>
-            <input type="text" class="form-control data-source-slugify" id="title" name="title" placeholder="Введите заголовок статьи" />
+            <input type="text" class="form-control data-source-slugify" id="title" name="title" placeholder="Введите заголовок статьи" value="{{ old('title') }}"/>
         </div>
         <div class="form-group">
             <label for="slug">Slug:</label>
-            <input type="text" class="form-control data-target-slugify" id="slug" name="slug" placeholder="-slug-" />
+            <input type="text" class="form-control data-target-slugify" id="slug" name="slug" placeholder="-slug-" value="{{ old('slug') }}"/>
         </div>
         <div class="form-group">
             <label for="excerpt">Краткое содержание:</label>
-            <textarea class="form-control" id="excerpt" rows="3" name="excerpt" placeholder="Краткое описание"></textarea>
+            <textarea class="form-control" id="excerpt" rows="3" name="excerpt" >{{ old('excerpt') }}</textarea>
         </div>
         <div class="form-group">
             <label for="excerpt">Текст статьи:</label>
-            <textarea class="form-control" id="body" rows="3" name="body"></textarea>
+            <textarea class="form-control" id="body" rows="3" name="body">{{ old('body') }}</textarea>
         </div>
         <div class="custom-control custom-checkbox form-group">
-            <input type="checkbox" class="custom-control-input" id="is_published" name="is_published">
+            <input type="checkbox" class="custom-control-input" id="is_published" name="is_published" {{ old('is_published') ? 'checked' : '' }}>
             <label class="custom-control-label" for="is_published">Опубликовать</label>
         </div>
         <div class="form-group">
