1ef7693974feffe3376474690f4df94754957b36
grogg19 <differ@list.ru>
2021-07-26 14:31:25 +0300

    ссылки генерируются функцией route()

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,10 +1,10 @@
 <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between">
-        <a class="p-2 text-muted" href="/">Главная</a>
-        <a class="p-2 text-muted" href="/about">О нас</a>
-        <a class="p-2 text-muted" href="/articles/create">Создать статью</a>
-        <a class="p-2 text-muted" href="/contacts">Контакты</a>
-        <a class="p-2 text-muted" href="/feedback">Список обращений</a>
+        <a class="p-2 text-muted" href="{{ route('mainPage') }}">Главная</a>
+        <a class="p-2 text-muted" href="{{ route('aboutPage') }}">О нас</a>
+        <a class="p-2 text-muted" href="{{ route('createArticle') }}">Создать статью</a>
+        <a class="p-2 text-muted" href="{{ route('contactsPage') }}">Контакты</a>
+        <a class="p-2 text-muted" href="{{ route('feedbackPage') }}">Список обращений</a>
         <a class="p-2 text-muted" href="#">Админ. раздел</a>
     </nav>
 </div>
