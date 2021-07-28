92b361b73de392493cf4e66229975cb34cf65814
grogg19 <differ@list.ru>
2021-07-28 12:39:37 +0300

    изменено имя создаваемой бд на laravel

--- db-init.sh
+++ db-init.sh
@@ -2,4 +2,4 @@
 service mysql start
 mysql -u root -e "CREATE USER 'dbuser'@'%' IDENTIFIED BY 'password';"
 mysql -u root -e "GRANT ALL PRIVILEGES  ON *.* TO 'dbuser'@'%';"
-mysql -u root -e "CREATE DATABASE cms;"
+mysql -u root -e "CREATE DATABASE laravel;"
