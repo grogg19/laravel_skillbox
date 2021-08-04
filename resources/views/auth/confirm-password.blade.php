efd31da31ccd727765ac8cabac996f6c28edd38c
grogg19 <differ@list.ru>
2021-08-04 23:49:26 +0300

    шаблоны для восстановления пароля

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,10 +1,8 @@
+@extends('layout.main_without_sidebar')
+
+@section('content')
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
@@ -34,3 +32,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+@endsection
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,10 +1,8 @@
+@extends('layout.main_without_sidebar')
+
+@section('content')
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
             {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
@@ -37,3 +35,4 @@
         </div>
     </x-auth-card>
 </x-guest-layout>
+@endsection
