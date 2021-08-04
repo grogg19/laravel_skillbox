9f1faf7787084d1e75a1d6f66db9cb8dee89eb54
grogg19 <differ@list.ru>
2021-08-04 19:28:04 +0300

    install Laravel Breeze

--- database/migrations/2021_07_23_101913_create_articles_table.php
+++ database/migrations/2021_07_23_101913_create_articles_table.php
@@ -15,6 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->unsignedInteger('id', true);
+            $table->unsignedInteger('owner_id')->nullable(false);
             $table->string('title', 191);
             $table->string('slug')->unique();
             $table->text('excerpt');
