6e0761dd52778a120d75ee166098e32e1b32f834
grogg19 <differ@list.ru>
2021-08-06 01:47:40 +0300

    правка шаблонов

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block font-weight-light text-small text-secondary ']) }} class="font-weight-light">
     {{ $value ?? $slot }}
 </label>
