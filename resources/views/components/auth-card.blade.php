a7145562c73b1d129112f8077f1d036642dc8b60
grogg19 <differ@list.ru>
2021-08-05 16:04:06 +0300

    delete laravel breeze

--- resources/views/components/auth-card.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 mt-5">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-lg overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
-</div>
