f042df2bc86becd461733ac5f4bb2a78789bc295
grogg19 <differ@list.ru>
2021-08-14 13:36:54 +0300

    добавлены новые директивы admin и  endadmin в Blade

---
+++ app/Providers/RoleServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\UserRolesChecker;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class RoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('admin', function () {
+            return '<?php if(!empty(auth()->user()->role->id) && auth()->user()->role->id == 1) { ?>';
+        });
+
+        Blade::directive('endadmin', function () {
+            return '<?php } ?>';
+        });
+    }
+}
