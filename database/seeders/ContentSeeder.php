607df896958d6b14852d7d53785876ee23750f97
grogg19 <differ@list.ru>
2021-08-17 01:10:35 +0300

    создан сидер ContentSeeder

---
+++ database/seeders/ContentSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = Tag::factory()->count(10)->create();
+
+        User::factory()->count(3)->create()->each(
+            function (User $user) use ($tags){
+                Article::factory()->count(10)->create([
+                    'owner_id' => $user
+                ])->each(function (Article $article) use ($tags){
+                    $article->tags()->attach(
+                        $tags
+                            ->shuffle()
+                            ->take(rand(1,4))
+                            ->pluck('id'));
+                });
+            }
+        );
+
+    }
+}
