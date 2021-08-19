9b4f34974e6b2e58a3dfba02332cdb9304f2e912
grogg19 <differ@list.ru>
2021-08-19 12:40:40 +0300

    правки по стилям

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Article\StoreArticleRequest;
 use App\Http\Requests\Tags\TagRequest;
 use App\Models\Article;
-use App\Models\Tag;
 use App\Repositories\ArticleRepositoryInterface;
 use App\Services\ArticleStore;
 use App\Services\TagsSynchronizer;
@@ -96,7 +95,7 @@ class ArticlesController extends Controller
         $article = is_numeric($articleKey) ? $this->articleRepository->getArticleById($articleKey)
             : $this->articleRepository->getArticleBySlug($articleKey);
 
-        if($article === null) {
+        if ($article === null) {
             return redirect(route('article.main'))
                 ->with('status', 'Такой статьи не существует!');
         }
--- app/helpers.php
+++ app/helpers.php
@@ -5,14 +5,14 @@ if (! function_exists('push_all')) {
     /**
      * @param null $title
      * @param null $text
-     * @param null $articleLink
+     * @param null $link
      * @return \Illuminate\Contracts\Foundation\Application|mixed
      */
-    function push_all($title = null, $text = null, $articleLink = null)
+    function push_all($title = null, $text = null, $link = null)
     {
-        if (is_null($title) || is_null($text) || is_null($articleLink)) {
+        if (is_null($title) || is_null($text)) {
             return app(\App\Services\PushAll::class);
         }
-        return app(\App\Services\PushAll::class)->send($title, $text, $articleLink);
+        return app(\App\Services\PushAll::class)->send($title, $text, $link);
     }
 }
