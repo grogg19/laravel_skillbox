3d63618424fc28d87d76f4810d13490dc77a614c
grogg19 <differ@list.ru>
2021-08-26 12:57:22 +0300

    ArticleHistory теперь наследуется от Pivot

--- app/Models/ArticleHistory.php
+++ app/Models/ArticleHistory.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ArticleHistory extends Model
+class ArticleHistory extends Pivot
 {
     use HasFactory;
 
