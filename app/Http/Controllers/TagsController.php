68496ff6d2853d2c71c1e833ae736083e8fb5deb
grogg19 <differ@list.ru>
2021-08-30 14:17:24 +0300

    создана страница с списками статей и новостей с выборкой по тегам

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -8,17 +8,22 @@ class TagsController extends Controller
 {
     /**
      * @param Tag $tag
-     * @param int $perPage
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function index(Tag $tag, int $perPage = 10)
+    public function index(Tag $tag)
     {
         $articles = $tag->articles()
             ->latest()
             ->where('is_published', 1)
             ->with('tags')
-            ->paginate($perPage);
+            ->get();
 
-        return view('index', compact('articles'));
+        $news = $tag->news()
+            ->latest()
+            ->where('is_published', 1)
+            ->with('tags')
+            ->get();
+
+        return view('tags.articles_news', compact('articles', 'news'));
     }
 }
--- resources/views/tags/list.blade.php
+++ resources/views/tags/list.blade.php
@@ -5,11 +5,7 @@
 @if ($tags->isNotEmpty())
     <div class="mb-2">
         @foreach($tags as $tag)
-            @if(isset($tag->pivot->taggable_type))
-                <a href="{{ route( 'tags.' . $tag->pivot->taggable_type . '.selectByTag', ['tag' => $tag]) }}" class="badge badge-secondary">{{ $tag->name }}</a>
-            @else
-                <a href="{{ route( 'tags.selectByTag', ['tag' => $tag]) }}" class="badge badge-secondary">{{ $tag->name }}</a>
-            @endif
+            <a href="{{ route( 'tags.selectByTag', ['tag' => $tag]) }}" class="badge badge-secondary">{{ $tag->name }}</a>
         @endforeach
     </div>
 @endif
