afb0abf59acb5266352b302235f692e9800313aa
grogg19 <differ@list.ru>
2021-09-07 00:54:25 +0300

    added styles and Vue

---
+++ resources/js/echo.js
@@ -0,0 +1,17 @@
+require('./lodash')
+
+import Echo from "laravel-echo"
+
+window.io = require('socket.io-client');
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001',
+});
+
+// Vue.component('task-update', require('./components/TaskUpdate').default)
+// Vue.component('chat', require('./components/Chat').default)
+
+const app = new Vue({
+    el: '#app'
+})
