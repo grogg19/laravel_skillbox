fbbe4cc97ee0ff3e78a9cb201d1b34bb56c0f0fd
grogg19 <differ@list.ru>
2021-09-10 01:07:24 +0300

    Кеширование сообщений

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * Class Message
@@ -14,4 +15,13 @@ class Message extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function () {
+            Cache::tags(['messages'])->flush();
+        });
+    }
 }
