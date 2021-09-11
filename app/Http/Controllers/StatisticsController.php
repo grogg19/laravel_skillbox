5d897590b117ddfdcd89bf1bc7d5f180bb306634
grogg19 <differ@list.ru>
2021-09-11 15:30:18 +0300

    убрано кеширование из статистики

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -6,131 +6,26 @@ use App\Repositories\ArticleRepositoryInterface;
 use App\Repositories\NewsRepositoryInterface;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Cache;
 
 class StatisticsController extends Controller
 {
-    public function index(
-        ArticleRepositoryInterface $articleRepository,
-        NewsRepositoryInterface $newsRepository,
-        UserRepositoryInterface $userRepository
-    )
+    public function index(ArticleRepositoryInterface $articleRepository, NewsRepositoryInterface $newsRepository, UserRepositoryInterface $userRepository)
     {
         $statistic = new Collection();
 
         // Количество статей и Новостей
-        $statistic
-            ->put(
-                'totalArticles',
-                Cache::tags(['articles', 'stats'])
-                    ->remember(
-                        'totalArticles',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getAllArticlesCount();
-                        }
-                        )
-            )
-        ;
-
-        $statistic
-            ->put(
-                'totalNews',
-                Cache::tags(['news', 'stats'])
-                    ->remember(
-                        'totalNews',
-                        3600 * 24,
-                        function () use ($newsRepository) {
-                            return $newsRepository->getAllNewsCount();
-                        }
-                        )
-            )
-        ;
+        $statistic->put('totalArticles', $articleRepository->getAllArticlesCount());
+        $statistic->put('totalNews', $newsRepository->getAllNewsCount());
 
         // самая длинная и самая короткая статья
-        $statistic
-            ->put(
-                'longestArticle',
-                Cache::tags(['articles', 'stats'])
-                    ->remember(
-                        'longestArticle',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getLongestArticle();
-                        }
-                        )
-            )
-        ;
+        $statistic->put('longestArticle', $articleRepository->getLongestArticle());
+        $statistic->put('shortestArticle', $articleRepository->getShortestArticle());
 
-        $statistic
-            ->put(
-                'shortestArticle',
-                Cache::tags(['articles', 'stats'])
-                    ->remember(
-                        'shortestArticle',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getShortestArticle();
-                        }
-                        )
-            )
-        ;
+        $statistic->put('authorManiac', $userRepository->getAuthorManiac());
+        $statistic->put('averageQuantityArticles', $articleRepository->getAverageQuantityArticlesActiveUser());
 
-        $statistic
-            ->put(
-                'authorManiac',
-                Cache::tags(['users', 'articles', 'stats'])
-                    ->remember(
-                        'authorManiac',
-                        3600 * 24,
-                        function () use ($userRepository) {
-                            return $userRepository->getAuthorManiac();
-                        }
-                        )
-            )
-        ;
-
-        $statistic
-            ->put(
-                'averageQuantityArticles',
-                Cache::tags(['articles', 'users', 'stats'])
-                    ->remember(
-                        'averageQuantityArticles',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getAverageQuantityArticlesActiveUser();
-                        }
-                        )
-            )
-        ;
-
-        $statistic
-            ->put(
-                'mostChangeableArticle',
-                Cache::tags(['articles', 'stats'])
-                    ->remember(
-                        'mostChangeableArticle',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getMostChangeableArticle();
-                        }
-                        )
-            )
-        ;
-
-        $statistic
-            ->put(
-                'mostDiscussableArticle',
-                Cache::tags(['articles', 'comments', 'stats'])
-                    ->remember(
-                        'mostDiscussableArticle',
-                        3600 * 24,
-                        function () use ($articleRepository) {
-                            return $articleRepository->getMostDiscussableArticle();
-                        }
-                        )
-            )
-        ;
+        $statistic->put('mostChangeableArticle', $articleRepository->getMostChangeableArticle());
+        $statistic->put('mostDiscussableArticle', $articleRepository->getMostDiscussableArticle());
 
         return view('statistics', $statistic);
     }
