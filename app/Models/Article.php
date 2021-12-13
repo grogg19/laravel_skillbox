a33e4b2929231523f57f1ee150a8c48a9bb81268
grogg19 <differ@list.ru>
2021-12-13 18:18:35 +0300

    make comments

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -34,6 +34,9 @@ class Article extends Model implements HasTags, HasComments
         'is_published' => 'boolean'
     ];
 
+    /**
+     * @return string
+     */
     public function getRouteKeyName()
     {
         return 'slug';
@@ -47,16 +50,25 @@ class Article extends Model implements HasTags, HasComments
         return $this->morphToMany(Tag::class, 'taggable');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function history()
     {
         return $this->belongsToMany(User::class, 'article_histories', 'article_id', 'user_id')
@@ -65,6 +77,9 @@ class Article extends Model implements HasTags, HasComments
             ->withTimestamps();
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function historyByPivot()
     {
         return $this->belongsToMany(User::class, 'article_histories', 'article_id', 'user_id')
@@ -74,18 +89,25 @@ class Article extends Model implements HasTags, HasComments
             ->orderByPivot('updated_at');
     }
 
+    /**
+     * @param $attributes
+     * @return void
+     */
     public function addComment($attributes)
     {
         $this->comments()->create($attributes);
     }
 
+    /**
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
 
         static::updating(function (Article $article) {
 
-            $after = $article->getDirty();
+            $after = $article->getDirty(); // получаем атрибуты, которые изменились с последней синхронизации
 
             $article->history()->attach(auth()->id(), [
                 'before' => Arr::only($article->fresh()->toArray(), array_keys($after)),
